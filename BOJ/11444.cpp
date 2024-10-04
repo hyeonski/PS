e69d805b904de9152380cd680d24a4f3c1d776c4
HyeonSeung Kim <hyeonski@student.42seoul.kr>
2024-10-04 14:08:49 +0000

    20241004

--- BOJ/11444.cpp
+++ BOJ/11444.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+#include <map>
+
+#define MOD 1000000007
 
 typedef unsigned long long int ull;
 
+std::map<ull, ull> m;
+
+ull fibo(ull n)
+{
+    if (m[n])
+    {
+        return m[n];
+    }
+
+    ull result;
+    if (n % 2 == 0)
+    {
+        result = (fibo(n / 2) * (fibo(n / 2 + 1) + fibo(n / 2 - 1))) % MOD;
+    }
+    else
+    {
+        result = ((fibo((n + 1) / 2) * fibo((n + 1) / 2)) % MOD) + ((fibo((n - 1) / 2) * fibo((n - 1) / 2)) % MOD) % MOD;
+    }
+
+    return m[n] = result % MOD;
+}
+
 int main()
 {
-    ull n; std::cin >> n;
-    ull arr[5];
-    arr[0] = 0;
-    arr[1] = 1;
-    for (ull i = 2; i <= n; ++i)
-    {
-        arr[i % 3] = (arr[(i-1) % 3] + arr[(i-2) % 3]) % 1000000007;
-    }
-    std::cout << arr[n % 3] << '\n';
+    ull n;
+    std::cin >> n;
+    m[0] = 0;
+    m[1] = 1;
+    m[2] = 1;
+    std::cout << fibo(n);
 }
