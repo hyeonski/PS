5c6ea49791728ee40131344bed54b9289b5348a1
hyeonski <hyeonski@student.42seoul.kr>
2026-04-05 20:16:57 +0900

    14891 review

--- BOJ/14891.py
+++ BOJ/14891.py
@@ -1,4 +1,4 @@
-gears = [input() for _ in range(4)]
+gears = [input().strip() for _ in range(4)]
 heads = [0, 0, 0, 0]
 k = int(input())
 
@@ -8,25 +8,26 @@ def rotate_one(i, clockwise):
     else:
         heads[i] = (heads[i] + 1) % 8
 
-visited = []
-def rotate(i, clockwise):
-    visited[i] = 1
-    if i != 0 and not visited[i-1]:
+def rotate(i, clockwise, visited):
+    visited[i] = True
+    if i > 0 and not visited[i - 1]:
         if gears[i][(heads[i] + 6) % 8] != gears[i - 1][(heads[i - 1] + 2) % 8]:
-            rotate(i - 1, not clockwise)
-    if i != 3 and not visited[i+1]:
+            rotate(i - 1, not clockwise, visited)
+    if i < 3 and not visited[i + 1]:
         if gears[i][(heads[i] + 2) % 8] != gears[i + 1][(heads[i + 1] + 6) % 8]:
-            rotate(i + 1, not clockwise)
+            rotate(i + 1, not clockwise, visited)
     rotate_one(i, clockwise)
 
 for _ in range(k):
-    gear, clockwise = input().split()
-    visited = [0, 0, 0, 0]
-    rotate(int(gear) - 1, True if int(clockwise) == 1 else False)
+    gear, direction = map(int, input().split())
+    visited = [False, False, False, False]
+    rotate(gear - 1, direction == 1, visited)
+
 score = 0
 for i in range(4):
     if gears[i][heads[i]] == '1':
-        score = score + (2 ** i)
+        score += 2 ** i
+
 print(score)
 
 
